e67dea9dcb24246fe3a50de1a77047fcb1f75565
John Clarke <john@algofi.org>
2022-10-26 06:55:53 +0000

    optimized speed of vebank update

--- governance/vebank_update.py
+++ governance/vebank_update.py
@@ -19,6 +19,7 @@ from algofipy.globals import Network
 from algofipy.governance.v1.governance_config import VOTING_ESCROW_STRINGS
 from algofipy.transaction_utils import wait_for_confirmation, TransactionGroup, get_default_params
 from algofipy.staking.v2.staking_config import STAKING_CONFIGS
+from algofipy.governance.v1.user_voting_escrow_state import UserVotingEscrowState
 
 def get_update_vebank_data_txns(client, sender, user_updating):
     params = get_default_params(client.algod)
@@ -45,7 +46,7 @@ def get_staking_update_boost_multiplier_txns(client, sender, user_updating, stak
     app_args = [bytes(UPDATE_TARGET_USER, "utf-8")]
     foreign_apps = [client.governance.voting_escrow.app_id]
     accounts = [user_updating]
-    txn1 = transaction.ApplicationNoOpTxn(sender, params, staking_app_id, app_args, foreign_apps=foreign_apps, accounts=accounts)
+    txn1 = ApplicationNoOpTxn(sender, params, staking_app_id, app_args, foreign_apps=foreign_apps, accounts=accounts)
 
     return TransactionGroup([txn0, txn1])
 
@@ -73,7 +74,7 @@ if __name__ == '__main__':
     parser.add_argument("--algod_token", type=str, default="")
     parser.add_argument("--indexer_uri", type=str, default="https://algoindexer.algoexplorerapi.io")
     parser.add_argument("--indexer_token", type=str, default="")
-    parser.add_argument("--pct_threshold", type=int, default=5)
+    parser.add_argument("--pct_threshold", type=int, default=0.5)
     parser.add_argument("--env_fpath", type=str, required=True)
     args = parser.parse_args()
 
@@ -87,24 +88,32 @@ if __name__ == '__main__':
 
     # update user vebank on voting escrow
     print("Querying governance users...")
-    governor_addresses = client.governance.get_governors()
-    governors = []
-    for address in governor_addresses:
-        user = client.governance.get_user(address)
-        governors.append(user)
+    (governor_admin_state, storage_mapping) = client.governance.get_governor_admin_state()
+    governor_voting_escrow_state = client.governance.get_governor_voting_escrow_state()
+    governor_addresses = list(set(list(governor_admin_state.keys()) + list(governor_voting_escrow_state.keys())))
+    governor_state = {}
+    for governor_address in governor_addresses:
+        admin_state = governor_admin_state.get(governor_address, {})
+        voting_escrow_state = governor_voting_escrow_state.get(governor_address, {})
+        if admin_state and voting_escrow_state:
+            governor_state[governor_address] = {
+                "admin": admin_state,
+                "voting_escrow": voting_escrow_state
+            }
+
 
     print("Updating veBANK...")
-    for governor in governors:
-        amount_vebank = governor.user_voting_escrow_state.amount_vebank
-        projected_vebank = client.governance.voting_escrow.get_projected_vebank_amount(governor.user_voting_escrow_state)
+    for governor_address in governor_state:
+        amount_vebank = governor_state[governor_address]["voting_escrow"][VOTING_ESCROW_STRINGS.user_amount_vebank]
+        projected_vebank = client.governance.voting_escrow.get_projected_vebank_amount(UserVotingEscrowState(governor_state[governor_address]["voting_escrow"]))
         if amount_vebank > 0:
             pct_change = (projected_vebank - amount_vebank) / amount_vebank * 100
             if pct_change < -args.pct_threshold:
-                print("Updating veBANK of " + governor.address + " which has fallen " + str(-pct_change) + " percent")
-                txn = get_update_vebank_data_txns(client, keeper, governor.address)
+                print("Updating veBANK of " + governor_address + " which has fallen " + str(-pct_change) + " percent")
+                txn = get_update_vebank_data_txns(client, keeper, governor_address)
                 txn.sign_with_private_key(keeper_key)
                 txn.submit(client.algod, wait=False)
-    
+
     # wait for txns to settle
     time.sleep(5)
     
@@ -113,19 +122,16 @@ if __name__ == '__main__':
     staking_contracts = STAKING_CONFIGS[Network.MAINNET]
     for staking_contract in staking_contracts:
         staking_app_id = staking_contract.app_id
-        user_addresses = get_accounts_opted_in_staking(client, staking_app_id)
-        governors = [(address, client.governance.get_user(address)) for address in user_addresses]
-        for (address, governor) in governors:
-            if not governor.opted_into_governance:
+        staking_data = client.staking.get_staking_state(staking_app_id)
+        for address in staking_data:
+            if not address in governor_state:
                 continue
-            staking_user = client.staking.get_user(address)
-            staking_user.load_state()
-            projected_boost_multiplier = client.governance.voting_escrow.get_projected_boost_multiplier(governor.user_voting_escrow_state)
-            staking_boost_multiplier = staking_user.user_staking_states[staking_app_id].boost_multiplier
+            projected_boost_multiplier = client.governance.voting_escrow.get_projected_boost_multiplier(UserVotingEscrowState(governor_state[address]["voting_escrow"]))
+            staking_boost_multiplier = staking_data[address]["boost_multiplier"]
             if staking_boost_multiplier > 0:
                 pct_change = (projected_boost_multiplier - staking_boost_multiplier) / staking_boost_multiplier * 100
                 if pct_change < -args.pct_threshold:
-                    print("Updating boost multiplier of " + governor.address + " which has fallen " + str(-pct_change) + " percent on " + str(staking_app_id))
-                    txn = get_staking_update_boost_multiplier_txns(client, keeper, governor.address, staking_app_id)
+                    print("Updating boost multiplier of " + address + " which has fallen " + str(-pct_change) + " percent on " + str(staking_app_id))
+                    txn = get_staking_update_boost_multiplier_txns(client, keeper, address, staking_app_id)
                     txn.sign_with_private_key(keeper_key)
                     txn.submit(client.algod, wait=False)
