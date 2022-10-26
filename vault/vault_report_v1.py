c106ffed1e6234474154cb9e634b379829fe2c2e
John Clarke <john@algofi.org>
2022-10-26 02:56:09 +0000

    added vault scripts

---
+++ vault/vault_report_v1.py
@@ -0,0 +1,129 @@
+
+# basic imports
+import sys, argparse
+import requests as re
+import pandas as pd
+from pytz import timezone
+from datetime import datetime, timedelta
+
+# algorand imports
+from algosdk import account, encoding, mnemonic
+from algosdk.v2client.algod import AlgodClient
+from algosdk.v2client.indexer import IndexerClient
+
+# algofi imports
+from algofi.v1.client import AlgofiMainnetClient
+
+def get_governor_data(slug):
+    url = "https://governance.algorand.foundation/api/periods/%s/governors/?limit=100" % (slug)
+    governor_data = {}
+    while url != None:
+        data = re.get(url).json()
+        url = data["next"]
+        results = data["results"]
+        for result in results:
+            governor_data[result["account"]["address"]] = {
+                "beneficiary_account": result["beneficiary_account"],
+                "committed_algo_amount": result["committed_algo_amount"],
+                "is_eligible": result["is_eligible"],
+                "voted": result["voted_voting_session_count"] > 0
+            }
+    return governor_data
+
+def get_time(tz="EST"):
+    tz = timezone(tz)
+    fmt = "%Y-%m-%d %H:%M:%S %Z%z"
+    ts = datetime.now(tz).strftime(fmt)
+    return ts
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Input processor")
+    parser.add_argument("--algod_uri", type=str, default="https://node.algoexplorerapi.io")
+    parser.add_argument("--algod_token", type=str, default="")
+    parser.add_argument("--indexer_uri", type=str, default="https://algoindexer.algoexplorerapi.io")
+    parser.add_argument("--indexer_token", type=str, default="")
+    parser.add_argument("--slug", type=str, required=True)
+    parser.add_argument("--csv_fpath", type=str, required=True)
+    args = parser.parse_args()
+
+    ts = get_time()
+    
+    # initialize clients
+    algod_client = AlgodClient(args.algod_token, args.algod_uri)
+    indexer_client = IndexerClient(args.indexer_token, args.indexer_uri)
+    algofi_client = AlgofiMainnetClient(algod_client, indexer_client)
+
+    vault_accounts = algofi_client.get_storage_accounts(verbose=True)
+    valgo_app_id = algofi_client.markets["vALGO"].market_app_id
+    vault_accounts_filtered = []
+    active_collateral = "dWFj"
+    # filter vault accounts based on supply to vALGO market
+    for vault_account in vault_accounts:
+        user_local_state = vault_account.get("apps-local-state", [])
+        for app_local_state in user_local_state:
+            if app_local_state["id"] == valgo_app_id:
+                fields = app_local_state.get("key-value", [])
+                for field in fields:
+                    if field["key"] == active_collateral:
+                        value = field["value"]["uint"]
+                        if value:
+                            vault_accounts_filtered.append(vault_account)
+
+    governor_data = get_governor_data(args.slug)
+    data_dict = {"Vault": [], "Balance": [], "Balance_Less_UnsyncAndMin": [], "Committment": [], "Buffer": [], "Eligible": [], "Voted": [], "Beneficiary": []}
+    for vault_account in vault_accounts_filtered:
+        vault_address = vault_account["address"]
+        balance = vault_account["amount"] / 1e12
+
+        # get supplied amount
+        user_local_state = vault_account.get("apps-local-state", [])
+        for app_local_state in user_local_state:
+            if app_local_state["id"] == valgo_app_id:
+                fields = app_local_state.get("key-value", [])
+                for field in fields:
+                    if field["key"] == active_collateral:
+                        supplied = field["value"]["uint"] / 1e12
+
+        if vault_address in governor_data:
+            committed_algo_amount = float(governor_data[vault_address]["committed_algo_amount"]) / 1e12
+            is_eligible = governor_data[vault_address]["is_eligible"]
+            voted = governor_data[vault_address]["voted"]
+            beneficiary_account = governor_data[vault_address]["beneficiary_account"]
+        else:
+            committed_algo_amount = 0
+            is_eligible = False
+            voted = False
+            beneficiary_account = None
+
+        data_dict["Vault"].append(vault_address)
+        data_dict["Balance"].append(balance)
+        data_dict["Balance_Less_UnsyncAndMin"].append(supplied)
+        data_dict["Committment"].append(committed_algo_amount)
+        data_dict["Buffer"].append(balance - committed_algo_amount)
+        data_dict["Eligible"].append(is_eligible)
+        data_dict["Voted"].append(voted)
+        data_dict["Beneficiary"].append(beneficiary_account)
+    governor_df = pd.DataFrame(data_dict)
+    governor_df["Balance"] = governor_df["Balance"].round(3)
+    governor_df["Balance_Less_UnsyncAndMin"] = governor_df["Balance_Less_UnsyncAndMin"].round(3)
+    governor_df["Committment"] = governor_df["Committment"].round(3)
+    governor_df["Buffer"] = governor_df["Buffer"].round(3)
+    governor_df = governor_df.sort_values(by=["Balance"], ascending=False)
+
+    num_vaults = governor_df.shape[0]
+    num_eligible = governor_df[governor_df["Eligible"]].shape[0]
+    num_voted = governor_df[governor_df["Voted"]].shape[0]
+    num_eligible_voted = governor_df[governor_df["Eligible"] & governor_df["Voted"]].shape[0]
+    vault_global_balance = governor_df["Balance"].sum()
+    vault_global_balance_less_unsyncandmin = governor_df["Balance_Less_UnsyncAndMin"].sum()
+    vault_global_balance_eligible = governor_df[governor_df["Eligible"]]["Balance"].sum()
+    summary_dict = {"Vaults": [num_vaults], "Eligible": [num_eligible], "Voted": [num_voted], "Eligible_Voted": [num_eligible_voted], "Vault_Balance [mm]": [vault_global_balance],
+                    "Vault_Balance_LessUnsyncAndMin [mm]": [vault_global_balance_less_unsyncandmin], "Vault_Balance_Eligible [mm]": [vault_global_balance_eligible]}
+    summary_df = pd.DataFrame(summary_dict)
+    summary_df["Vault_Balance [mm]"] = summary_df["Vault_Balance [mm]"].round(3)
+    summary_df["Vault_Balance_LessUnsyncAndMin [mm]"] = summary_df["Vault_Balance_LessUnsyncAndMin [mm]"].round(3)
+    summary_df["Vault_Balance_Eligible [mm]"] = summary_df["Vault_Balance_Eligible [mm]"].round(3)
+    governor_df["Timestamp"] = ts
+
+    governor_df.to_csv(args.csv_fpath + "v1-vault-governors-%s.csv" % ts)
+    summary_df.to_csv(args.csv_fpath + "v1-vault-summary-%s.csv" % ts)
